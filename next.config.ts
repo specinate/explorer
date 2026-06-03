f1950c48b52c2b55c66f8efb24953cfcadc6d6eb
Coriano Harris <corianoharrispro@gmail.com>
2026-06-02 22:16:40 -0500

    fix: deployment build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export",
   images: {
     unoptimized: true,
   },
@@ -10,9 +8,6 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: true,
-  turbopack: {
-    resolveExtensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
-  },
 };
 
 export default nextConfig;
